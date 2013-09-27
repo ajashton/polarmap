889ecec6a22f453db29ccfc5c1d0197c62824a9c
AJ Ashton <aj.ashton@gmail.com>
2013-09-27 18:42:39 -0400

    update import script

--- import_data.sh
+++ import_data.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 
+DATADIR=$HOME/osmdata
+
 EPSG=3395  # should be both a valid EPSG code and valid PostGIS SRID
 BBOX="-180,60,180,90"  # latlon xmin,ymin,xmax,ymax
 BBOX_OGR=$(sed 's/,/ /g' <<< $BBOX)  # like $BBOX, but no commas
+MAX_COAST_AGE=$(bc <<< '24*60*60')
 
-get_planet() {
+# If we don't yet have an arctic extract to work with, download the latest
+# planet and create one with osmcovert.
+if [ ! -e arctic_latest.osm.pbf ]; then
     wget -O planet.md5 http://planet.openstreetmap.org/pbf/planet-latest.osm.pbf.md5
     planet=$(sed 's/.* //' < planet.md5)
-    test -e $latest || wget http://planet.openstreetmap.org/pbf/$planet
+    test -e $planet || wget http://planet.openstreetmap.org/pbf/$planet
     md5sum -c planet.md5 || exit 1
-}
-
-cut_arctic() {
     # Extracting the arctic from the planet takes ~30min with osmconvert
     # and results in a ~600MB file.
-    arctic=arctic$(sed 's/planet//' <<< "$planet")
-    ~/src/osmconvert $planet -b=$BBOX --complex-ways $arctic
-}
+    ~/src/osmconvert $planet -b=$BBOX --complex-ways arctic_latest.osm.pbf
+fi
 
-update_arctic() {
-    local dst=arctic_updated.pbf
-    if [ -e $dst ]; then
-        mv -f $dst arctic_old.pbf
-        local src=arctic_old.pbf
-    else
-        local src=$arctic
-    fi
-    # Updating the arctic should take only a few minutes for each days
-    # worth of changes to apply
-    osmupdate -b= $src $dst
-}
+# Updating the arctic extract to the latest OSM data with osmupdate.
+# This should take only a few minutes for each day's worth of changes.
+mv -f arctic_latest.osm.pbf arctic_old.osm.pbf
+osmupdate -b=$BBOX arctic_old.osm.pbf arctic_latest.osm.pbf
 
-gen_coastline() {
+# Download, reproject, and clip the latest coastline file, but only if our
+# existing copy is older than $MAX_COASTLINE_AGE.
+if [ ! -e land-polygons-split-4326.zip -o
+        $(($(date +%s)-$(stat -c '%Y' "$coastzip"))) -gt $MAX_COAST_AGE ]
+then
     wget -N http://data.openstreetmapdata.com/land-polygons-split-4326.zip
     unzip -f land-polygons-split-4326.zip
     ogr2ogr -t_srs EPSG:$EPSG -clipsrc $BBOX_OGR \
         arctic_land.shp land-polygons-split-4326/land_polygons.shp
-    shapeindex arctic_land.shp
-}
+fi
 
 import_arctic() {
     osm2pgsql \
@@ -49,3 +45,4 @@ import_arctic() {
         --hstore \
         $planet
 }
+
