b8bcaab64379d413fbd4771b2c7c82526514e68a
AJ Ashton <aj.ashton@gmail.com>
2015-07-19 17:12:35 -0400

    refactor import

--- import_data.sh
+++ import_data.sh
@@ -1,65 +1,96 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-# This script loads a database with an up-to-date extract of OSM data for
-# the arctic. If an arctic extract file is not locally available, it will
-# download the latest weekly OSM Planet dump and create one.
-#
-# This file will also fetch and process any additional required data such as
-# coastline shapefiles. If these already exist locally but are out of date,
-# they will be updated.
-
-DATADIR=$HOME/osmdata
-
-EPSG=3395   # should be both a valid EPSG code and valid PostGIS SRID
-BBOX="-180,60,180,90"           # latlon xmin,ymin,xmax,ymax
+PBF_URL="${PBF_URL:-http://planet.openstreetmap.org/pbf/planet-latest.osm.pbf}"
+DATADIR=./data
+EPSG=3995   # should be both a valid EPSG code and valid PostGIS SRID
+BBOX="-180,45,180,90"           # latlon xmin,ymin,xmax,ymax
 BBOX_OGR=${BBOX//,/ }           # like $BBOX, but no commas
-MAX_COAST_AGE=$((24*60*60))     # in seconds
+PG_DBNAME=${PG_DBNAME:-openpolarmap}
+PG_USER=${PG_USER:-postgres}
+PG_HOST=${PG_HOST:-localhost}
+PG_PORT=${PG_PORT:-5432}
+PSQL="psql -U $PG_USER -h $PG_HOST -p $PG_PORT"
+PSQLD="$PSQL -d $PG_DBNAME"
 
-mkdir -p $DATADIR
-cd $DATADIR
+pushd "$(pwd)"
+cd "$DATADIR"
 
-# If we don't yet have an arctic extract to work with, download the latest
-# planet and create one with osmcovert.
-if [ ! -e arctic_latest.osm.pbf ]; then
-    wget -O planet.md5 http://planet.openstreetmap.org/pbf/planet-latest.osm.pbf.md5
-    planet=$(sed 's/.* //' < planet.md5)
-    test -e $planet || wget http://planet.openstreetmap.org/pbf/$planet
-    md5sum -c planet.md5 || exit 1
-    # Extracting the arctic from the planet takes ~30min with osmconvert
-    # and results in a ~600MB file.
-    echo -n "Creating arctic extract... "
-    osmconvert $planet -b=$BBOX --complex-ways -o=arctic_latest.osm.pbf
-    echo "done."
+if [ ! -e planet.osm.pbf ]; then
+    wget -O planet.osm.pbf "$PBF_URL"
 fi
 
-# Updating the arctic extract to the latest OSM data with osmupdate.
-# This should take only a few minutes for each day's worth of changes.
-mv -f arctic_latest.osm.pbf arctic_old.osm.pbf
-echo -n "Updating OSM data... "
-osmupdate arctic_old.osm.pbf arctic_latest.osm.pbf -b=$BBOX
-echo "done."
+if [ ! -e arctic_latest.osm.pbf ]; then
+    osmconvert planet.osm.pbf -b="$BBOX" --complex-ways -o=arctic_latest.osm.pbf
+fi
 
-# Download, reproject, and clip the latest coastline file, but only if our
-# existing copy is older than $MAX_COASTLINE_AGE.
-coastzip=land-polygons-split-4326.zip
-if [ ! -e $coastzip -o \
-    $(($(date +%s)-$(stat -c '%Y' "$coastzip"))) -gt $MAX_COAST_AGE ]
+if [ $(($(date +%s)-$(stat -c '%Y' arctic_latest.osm.pbf))) -gt $((12*60*60)) ]
 then
-    wget -N http://data.openstreetmapdata.com/$coastzip
-    unzip -f $coastzip
+    # Updating the arctic extract to the latest OSM data with osmupdate.
+    # This should take only a few minutes for each day's worth of changes.
+    osmupdate arctic_latest.osm.pbf arctic_new.osm.pbf -b=$BBOX
+    mv -f arctic_latest.osm.pbf arctic_old.osm.pbf
+    mv -f arctic_new.osm.pbf arctic_latest.osm.pbf
+fi
+
+if [ ! -e arctic_land.shp -o \
+    $(($(date +%s)-$(stat -c '%Y' arctic_land.shp))) -gt $((48*60*60)) ]
+then
+    wget -N http://data.openstreetmapdata.com/land-polygons-complete-4326.zip
+    unzip -ju land-polygons-complete-4326.zip
     ogr2ogr -t_srs EPSG:$EPSG -clipsrc $BBOX_OGR \
         arctic_land.shp land-polygons-split-4326/land_polygons.shp
 fi
 
-import_arctic() {
-    osm2pgsql \
-        --create \
-        --prefix=osm_arctic \
-        --proj=$EPSG \
-        --database=osm \
-        --username=postgres \
-        --hstore \
-        $planet
-}
+if [ ! -e natural_earth_vector.sqlite ]; then
+    nezip=natural_earth_vector.sqlite.zip
+    wget -O "$nezip" "http://naciscdn.org/naturalearth/packages/$nezip"
+    unzip -ju $nezip natural_earth_vector.sqlite
+fi
 
+if [ ! -e natural_earth_arctic.sqlite ]; then
+    ogr2ogr \
+        -clipsrc $BBOX_OGR \
+        -f SQLite \
+        natural_earth_arctic.sqlite \
+        natural_earth_vector.sqlite \
+            ne_10m_geographic_lines \
+            ne_10m_geography_marine_polys \
+            ne_10m_geography_regions_polys \
+            ne_10m_glaciated_areas \
+            ne_10m_land \
+            ne_10m_ocean \
+            ne_10m_populated_places_simple \
+            ne_10m_ports \
+            ne_10m_time_zones \
+            ne_10m_urban_areas
+fi
+
+## IMPORT
+
+$PSQL -c "drop database $PG_DBNAME;" || true
+$PSQL -c "create database $PG_DBNAME;"
+$PSQLD -c "create extension if not exists postgis;"
+$PSQLD -c "create extension if not exists hstore;"
+
+export PG_USE_COPY=TRUE
+export OGR_ENABLE_PARTIAL_REPROJECTION=TRUE
+export PGCLIENTENCODING=LATIN1
+ogr2ogr \
+    -s_srs EPSG:4326 \
+    -t_srs EPSG:$EPSG \
+    -segmentize 0.25 \
+    -f PostgreSQL \
+    "PG:user=$PG_USER dbname=$PG_DBNAME host=$PG_HOST port=$PG_PORT" \
+    natural_earth_arctic.sqlite \
+    -lco GEOMETRY_NAME=geom \
+    -nlt GEOMETRY
+
+export PGCLIENTENCODING=UTF8
+osm2pgsql \
+    --create \
+    --proj=$EPSG \
+    --database="$PG_DBNAME" \
+    --username="$PG_USER" \
+    --hstore \
+    arctic_latest.osm.pbf
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,38 +1,89 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-SRCDIR=$HOME/src
+SRCDIR=${TEMP:-/tmp}/src
 
 apt_packages=(
-    build-essential
-    libbz2-dev          # for building osm2pgsql
-    libgeos++-dev       # for building osm2pgsql
-    libmapnik
-    libmapnik-dev
-    libprotobuf-dev     # for building osm2pgsql, osmium
     postgresql-9.3-postgis-2.1
-    zlib1g-dev          # for building osmconvert
+    # for building osm2pgsql:
+    autoconf
+    automake
+    libtool
+    make
+    g++
+    pkg-config
+    libboost-dev
+    libboost-system-dev
+    libboost-filesystem-dev
+    libboost-thread-dev
+    libexpat1-dev
+    libgeos-dev
+    libgeos++-dev
+    libpq-dev
+    libbz2-dev
+    libproj-dev
+    zlib1g-dev  # also needed to build osmconvert
+    protobuf-compiler
+    libprotobuf-dev
+    lua5.2
+    liblua5.2-dev
 )
 
 install_packages() {
     sudo apt-get update
-    sudo apt-get install -y ${apt_packages[@]}
+    sudo apt-get install -y "${apt_packages[@]}"
 }
 
+install_node() {
+    wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
+    git clone https://github.com/creationix/nvm.git ~/.nvm
+    pushd ~/.nvm
+    git checkout "$(git describe --abbrev=0 --tags)"
+    popd
+    source ~/.nvm/nvm.sh
+    nvm install v0.10
+    nvm use v0.10
+}
 
 build_osmconvert() {
     wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o osmconvert
-    sudo cp osmconvert /usr/local/bin/
+    sudo mv osmconvert /usr/local/bin/
 }
 
 build_osmupdate() {
     wget -O - http://m.m.i24.cc/osmupdate.c | cc -x c - -o osmupdate
-    sudo cp osmupdate /usr/local/bin/
+    sudo mv osmupdate /usr/local/bin/
+}
+
+build_osm2pgsql() {
+    pushd "$(pwd)"
+    git clone https://github.com/openstreetmap/osm2pgsql.git osm2pgsql
+    cd osm2pgsql
+    ./autogen.sh
+    ./configure
+    make -j8
+    sudo make install
+    popd
 }
 
 # Do it
 install_packages
-mkdir -p $SRCDIR
-cd $SRCDIR
-build_osmconvert
-build_osmupdate
+mkdir -p "$SRCDIR"
+cd "$SRCDIR"
+
+if [ -z "$(which node)" ]; then
+    install_node
+fi
+
+if [ -z "$(which osmconvert)" ]; then
+    build_osmconvert
+fi
+
+if [ -z "$(which osmupdate)" ]; then
+    build_osmupdate
+fi
+
+if [ -z "$(which osm2pgsql)" ]; then
+    build_osm2pgsql
+fi
+
