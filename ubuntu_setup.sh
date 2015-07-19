d242085a747a19d6fee7170d84f688cec479a7bf
AJ Ashton <aj.ashton@gmail.com>
2015-07-19 18:10:46 -0400

    don't actually need node

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -34,17 +34,6 @@ install_packages() {
     sudo apt-get install -y "${apt_packages[@]}"
 }
 
-install_node() {
-    wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
-    git clone https://github.com/creationix/nvm.git ~/.nvm
-    pushd ~/.nvm
-    git checkout "$(git describe --abbrev=0 --tags)"
-    popd
-    source ~/.nvm/nvm.sh
-    nvm install v0.10
-    nvm use v0.10
-}
-
 build_osmconvert() {
     wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o osmconvert
     sudo mv osmconvert /usr/local/bin/
@@ -70,10 +59,6 @@ build_osm2pgsql() {
 install_packages
 mkdir -p "$SRCDIR"
 cd "$SRCDIR"
-
-if [ -z "$(which node)" ]; then
-    install_node
-fi
 
 if [ -z "$(which osmconvert)" ]; then
     build_osmconvert
